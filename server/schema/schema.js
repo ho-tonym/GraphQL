c92d98037934311bddddcc1eef616a6fa37381fc
ho-tonym <ho.tonym@gmail.com>
2019-08-16 19:00:05 -0400

    create GraphQL schema for author

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql')
 const _ = require('lodash')
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql
 
 // dummy data
 const books = [
@@ -10,28 +10,60 @@ const books = [
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ]
 
+const authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
+];
+
 // wrapped in es6 function that return grpahql object
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
 })
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
+
+
+// dont need to wrap fields here in a functio because order isnt important
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLString } }, // argumemt when querying a book
+      resolve(parent, args) { // parent- realtes to db | args- defined above
+        return _.find(books, { id: args.id })// code to get data from db
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(books, { id: args.id })
+        return _.find(authors, { id: args.id });
       },
     },
   },
 })
+// defines how we can jump into the graph to get data
+
+// book query which can be found in book object type
+// book(id: "2"){
+//   name
+//   genre
+// }
+
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
